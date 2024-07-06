0b900f5f61cb01c139b36fe95187bb7030b01a5d
Rostik <126328154+gab1m4ru@users.noreply.github.com>
2024-07-06 13:59:44 +0300

    Add files via upload

--- styles/js/script.js
+++ styles/js/script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded',
     function () {
         const navItems = document
             .querySelectorAll('.nav-item');
- 
+
         navItems.forEach(item => {
             item.addEventListener('click',
                 function () {
@@ -12,3 +12,14 @@ document.addEventListener('DOMContentLoaded',
                 });
         });
     });
+
+const form = document.querySelector('.form');
+const input = document.querySelector('.form__input');
+    
+input.addEventListener('focus', function () {
+  form.classList.add('form--active');
+});
+    
+input.addEventListener('blur', function () {
+  form.classList.remove('form--active');
+});
